5a1a9d527cc42fed441c23c9d6c634ded13e9082
chikuwait <chikuwa.1703@gmail.com>
2017-04-20 15:58:19 +0900

    fix uint64_t

--- include/stdint.h
+++ include/stdint.h
@@ -28,7 +28,7 @@
  */
 
 typedef long int intptr_t;
-typedef unsigned long long uint64_t;
+typedef unsigned long long int uint64_t;
 typedef unsigned int       uint32_t;
 typedef unsigned short     uint16_t;
 typedef unsigned char      uint8_t;
