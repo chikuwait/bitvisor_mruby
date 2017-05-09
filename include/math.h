af600083b133c158ac618fcda0d93df18dc1c7c0
chikuwait <chikuwa.1703@gmail.com>
2017-05-09 17:26:51 +0900

    write log10

--- include/math.h
+++ include/math.h
@@ -1,9 +1,10 @@
 #ifndef _BITVISOR_MATH_H
 #define _BITVISOR_MATH_H
+#include <nan.h>
 
 #define signbit(a) __builtin_signbit(a)
 #define frexp(a, b) __builtin_frexp(a, b)
-#define isfinite(a) __builtin_fisfinite(a)
+#define isfinite(a) __builtin_isfinite(a)
 #define pow(a, b) __builtin_pow(a, b)
 #define isnan(a) __builtin_isnan(a)
 #define isinf(a) __builtin_isinf(a)
@@ -13,5 +14,7 @@
 
 #define MAN __builtin_nan("")
 #define INFINITY __builtin_huge_valf()
+
+log10(long double __x){ return __builtin_log10l(__x); }
 
 #endif
