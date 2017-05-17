a00baa2b085e2cb5b2b8039d9922ef7f545a4abf
chikuwait <chikuwa.1703@gmail.com>
2017-05-18 00:51:44 +0900

    fix errno and write ERANGE

--- include/errno.h
+++ include/errno.h
@@ -1,6 +1,8 @@
 #ifndef _BITVISOR_ERRORNO_H
 #define _BITVISOR_ERRORNO_H
 
-#define errno 0
+extern int *__errno_location (void) __attribute__ ((__const__));
+#define errno (*__errno_location ())
+#define ERANGE 34
 #endif
 
