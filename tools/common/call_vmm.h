7d8987b387e20a197668440f19cc8b7f876ec8b2
chikuwait <chikuwa.1703@gmail.com>
2017-06-21 15:15:05 +0900

    revert : Mar 9, 2017(ac97e4b1051f9c5189a79533795ff572fb55ac2f)

---
+++ tools/common/call_vmm.h
@@ -0,0 +1,45 @@
+#include <stdint.h>
+#define CALL_VMM_GET_FUNCTION(name, ret) do { \
+	intptr_t call_vmm__addr0, call_vmm__addr1; \
+ \
+	asm volatile ("jmp 2f\n" \
+		      "0: vmcall; jmp *%1; .org 0b+5; vmmcall; jmp *%1;" \
+		      "   .org 0b+10; .string \"" name "\"\n" \
+		      "1: vmcall; jmp *%1; .org 1b+5; vmmcall; jmp *%1;" \
+		      "   .org 1b+10; .string \"" name "\"\n" \
+		      "2: mov $0b,%0; mov $1b,%1" \
+		      : "=r" (call_vmm__addr0), "=S" (call_vmm__addr1)); \
+	call_vmm_get_function (call_vmm__addr0, call_vmm__addr1, \
+			       5, 10, (ret)); \
+} while (0)
+
+typedef struct {
+	int vmmcall_number;
+	int vmmcall_type;
+} call_vmm_function_t;
+
+typedef struct {
+	intptr_t rbx, rcx, rdx, rsi, rdi;
+} call_vmm_arg_t;
+
+typedef struct {
+	intptr_t rax, rbx, rcx, rdx, rsi, rdi;
+} call_vmm_ret_t;
+
+static inline int
+call_vmm_function_no_vmm (call_vmm_function_t *f)
+{
+	return !f->vmmcall_type;
+}
+
+static inline int
+call_vmm_function_callable (call_vmm_function_t *f)
+{
+	return f->vmmcall_type && f->vmmcall_number;
+}
+
+int call_vmm_docall (void (*func) (void *data), void *data);
+void call_vmm_get_function (intptr_t addr0, intptr_t addr1,
+			    int aoff, int off, call_vmm_function_t *function);
+void call_vmm_call_function (call_vmm_function_t *function,
+			     call_vmm_arg_t *arg, call_vmm_ret_t *ret);
