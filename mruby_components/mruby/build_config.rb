30b6493b8f8744fc32a347f5c997ebeb6a6234d5
chikuwait <chikuwa.1703@gmail.com>
2017-08-29 17:53:51 +0900

    fix linker config

--- mruby_components/mruby/build_config.rb
+++ mruby_components/mruby/build_config.rb
@@ -7,6 +7,20 @@ MRuby::Build.new do |conf|
     linker.libraries = %w(softfloat)
     linker.library_paths = ["./"]
   end
-
  # conf.gembox 'default'
 end
+MRuby::CrossBuild.new('BitVisor') do |conf|
+   toolchain :gcc
+   conf.linker do |linker|
+    linker.libraries = %w(softfloat)
+    linker.library_paths = ["./"]
+   end
+
+   conf.cc.include_paths << "include/bitvisor"
+   conf.cc.flags << "-mcmodel=kernel -mno-red-zone -mfpmath=387 -mno-sse -mno-sse2 -mno-mmx -mno-3dnow -msoft-float -fno-asynchronous-unwind-tables -fno-omit-frame-pointer -fno-stack-protector"
+   conf.cc.defines << %w(DISABLE_STDIO)
+   conf.cc.defines << %w(DISABLE_FLOAT)
+   conf.cc.defines << %w(MRB_INT64)
+   conf.bins = []
+end
+
