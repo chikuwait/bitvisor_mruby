54ff1f2080ff5819a3c2cf3f68205048bef34a3a
chikuwait <chikuwa.1703@gmail.com>
2018-04-24 18:30:57 +0900

    add printf

--- process/mruby_core/include/bitvisor/printf.h
+++ process/mruby_core/include/bitvisor/printf.h
@@ -3,5 +3,6 @@ typedef unsigned long size_t;
 FILE stderr;
 int snprintf (char *str, size_t size, const char *format, ...);
 void fprintf (FILE *fp, const char *format, ...);
+int printf(const char *fmt, ...);// __attribute__ ((format (printf, 1, 2)));
 
 
