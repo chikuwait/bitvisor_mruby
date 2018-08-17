305ff7d16115d4919cb7f9b2775011bd6d3e7ea0
chikuwait <chikuwa.1703@gmail.com>
2018-08-17 18:25:28 +0900

    [feat]Addition of function to read Ethernet header from mruby

--- process/test.rb
+++ process/test.rb
@@ -1,3 +1,8 @@
 def helloworld(name = "no name")
   Bitvisor.print "Hello,#{name}-san!\n"
 end
+
+def readEthernetFreame
+  macaddr = Bitvisor.readBinary.map{|i| i.to_s(16)}.join(":")
+  Bitvisor.print"Destination mac address =#{macaddr}\n"
+end
